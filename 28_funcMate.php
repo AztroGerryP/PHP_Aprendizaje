8854f57f727d3c59705abc14e656f0b90a43ec7b
AztroGerryP <gerardogrduno@gmail.com>
2025-04-06 14:36:59 -0600

    "scope de una funcion"

---
+++ 28_funcMate.php
@@ -0,0 +1,31 @@
+<?php
+
+$numero = 15.765;
+
+// Redondea un numero a numeros mayores .5
+       // menores .1 a .4
+echo round($numero);
+echo "<br />";
+
+// Un parametro para que muestre los decimales
+echo round($numero, 1);
+echo "<br />";
+
+// Generar un numero aleatorio entre dos nuermos
+echo rand(1,10);
+echo "<br/>";
+
+// Redondea un numeros para arriva
+echo ceil($numero);
+echo "<br />";
+
+// constante por defecto de PI
+echo M_PI;
+
+
+/*
+Refencia de funciones matematicas
+https://www.php.net/manual/es/ref.math.php
+*/ 
+
+?>
---
+++ 29_scopePHP.php
@@ -0,0 +1,23 @@
+<?php
+
+// AMBITO DE LAS VARIABLES 
+   // funcion no puede acceder a una variable que este fuera
+   // no se puede acceder a una variable que esta dentro de ella
+
+$numero = 7; // GLOBAL POR LO QUE DEBE SER PARAMETRO
+
+function mostrarNumero($numero){
+    echo $numero; 
+}
+
+mostrarNumero($numero);
+
+
+function mostrarNumero1(){
+    $numero1 = 13; // LOCAL no se puede acceder a ella 
+    return $numero1
+}
+
+echo mostrarNumero1();
+
+?>
