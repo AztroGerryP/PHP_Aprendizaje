ca52a51b6f47aab8332a0e26e77de892ad6513b4
AztroGerryP <gerardogrduno@gmail.com>
2025-04-06 13:32:14 -0600

    "funciones sin parametro y con parametros, valor return"

---
+++ 23_funciones.php
@@ -0,0 +1,22 @@
+<?php
+
+// algunas funciones que se han estudiado
+// count();
+// sort(); 
+// rsort(); 
+// var_dump();
+// print_r();
+
+// bloque de codigo que se quiere usar
+function saludo(){
+    echo 'Hola mundo';
+}
+
+// llamar a la funcion 
+saludo();
+echo '<br/>';
+saludo();
+
+//count($nuevo); // es un parametro
+
+?>
---
+++ 24_retornoFuncion.php
@@ -0,0 +1,13 @@
+<?php
+              // parametros
+function sumar($numero1, $numero2){
+    $resultado = $numero1 + $numero2;
+    return $resultado;
+    // almacenar un valor
+}
+
+$res = sumar(12, 34);
+echo "La suma es $res <br/>";
+
+
+?>
