7f215d64ff2f7f1ef9871562798810b4c32c16ad
AztroGerryP <gerardogrduno@gmail.com>
2025-04-06 12:35:33 -0600

    "print_r mostrar datos al usuario"

---
+++ 22_printr.php
@@ -0,0 +1,28 @@
+<?php
+
+$texto = 'Hola';
+$numero = 564;
+$numero2 = 56.3;
+$arreglo = array('Jose', 'Pedro', 'Rosa', 'Maria');
+$arreglo_asociativo = array('nombre' => 'Gerardo', 'edad' => 38);
+$booleano = true;
+
+// valores legibles para los humanos
+print_r($texto);
+echo "<br/>";
+
+print_r($numero);
+echo "<br/>";
+
+print_r($numero2);
+echo "<br/>";
+
+print_r($arreglo);
+echo "<br/>";
+
+print_r($arreglo_asociativo);
+echo "<br/>";
+
+print_r($booleano);
+echo "<br/>";
+?>
